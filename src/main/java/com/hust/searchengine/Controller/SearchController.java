c347b0ac8274b2e6e23a4aa8bf6d862f2bd7aa92
guojingxing <33157458+Guojingxing@users.noreply.github.com>
2021-01-13 17:11:34 +0800

    version 2.0

--- src/main/java/com/hust/searchengine/Controller/SearchController.java
+++ src/main/java/com/hust/searchengine/Controller/SearchController.java
@@ -2,8 +2,9 @@ package com.hust.searchengine.Controller;
 
 import com.hust.searchengine.Entity.ClassInfo;
 import com.hust.searchengine.Entity.Student;
+import com.hust.searchengine.Entity.User;
 import com.hust.searchengine.Service.ClassInfoService;
-import com.hust.searchengine.Service.StudentService;
+import com.hust.searchengine.Service.SearchService;
 import com.hust.searchengine.Utils.FileUtil;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,66 +13,139 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 @RequestMapping("search")
-public class StudentController {
+public class SearchController {
 // http://localhost:8080/search
     @Autowired
-    private StudentService studentService;
+    private SearchService searchService;
 
     @Autowired
     private ClassInfoService classInfoService;
 
     @RequestMapping("main")
-    public String MainPage(){
-        return "main";
+    public String MainPage(HttpSession session){
+        User user = (User)session.getAttribute("user");
+        if(user!=null)
+            return "main";
+        else
+            return "redirect:/search/login";
     }
 
     @RequestMapping("advancedsearch")
-    public String AdvancedSearchPage(){
-        return "advancedsearch";
+    public String AdvancedSearchPage(HttpSession session){
+        User user = (User)session.getAttribute("user");
+        if(user!=null)
+            return "advancedsearch";
+        else
+            return "redirect:/search/login";
     }
 
     @RequestMapping("bookmark")
-    public String BookmarkPage(){
-        return "bookmark";
+    public String BookmarkPage(HttpSession session){
+        User user = (User)session.getAttribute("user");
+        if(user!=null)
+            return "bookmark";
+        else
+            return "redirect:/search/login";
     }
 
+    //登出
     @RequestMapping("logout")
-    public String LogoutPage(){
-        return "logout";
+    public String LogoutPage(HttpSession session){
+        User user = (User)session.getAttribute("user");
+        if(user!=null){
+            session.removeAttribute("user");
+        }
+        return "redirect:/search/login";
     }
 
+    //我的
     @RequestMapping("mine")
-    public String MinePage(){
-        return "mine";
+    public String MinePage(HttpSession session){
+        User user = (User)session.getAttribute("user");
+        if(user!=null)
+            return "mine";
+        else
+            return "redirect:/search/login";
     }
 
+    //个人主页
     @RequestMapping("profile")
-    public String ProfilePage(){
-        return "profile";
+    public String ProfilePage(HttpSession session){
+        User user = (User)session.getAttribute("user");
+        if(user!=null)
+            return "profile";
+        else
+            return "redirect:/search/login";
     }
 
+    //搜索
     @RequestMapping("search")
-    public String SearchPage(){
-        return "search";
+    public String SearchPage(HttpSession session){
+        User user = (User)session.getAttribute("user");
+        if(user!=null)
+            return "search";
+        else
+            return "redirect:/search/login";
     }
 
+    //设置
     @RequestMapping("settings")
-    public String SettingsPage(){
-        return "settings";
+    public String SettingsPage(HttpSession session){
+        User user = (User)session.getAttribute("user");
+        if(user!=null)
+            return "settings";
+        else
+            return "redirect:/search/login";
     }
 
+    //订阅
     @RequestMapping("subscription")
-    public String SubscriptionPage(){
-        return "subscription";
+    public String SubscriptionPage(HttpSession session){
+        User user = (User)session.getAttribute("user");
+        if(user!=null)
+            return "subscription";
+        else
+            return "redirect:/search/login";
     }
 
+    //热搜
     @RequestMapping("trends")
-    public String TrendsPage(){
-        return "trends";
+    public String TrendsPage(HttpSession session){
+        User user = (User)session.getAttribute("user");
+        if(user!=null)
+            return "trends";
+        else
+            return "redirect:/search/login";
+    }
+
+    //登录界面
+    @RequestMapping("login")
+    public String loginPage(HttpSession session){
+        User user = (User)session.getAttribute("user");
+
+        if(user!=null)
+            return "redirect:/search/main";
+        return "login";
+    }
+
+    //登录界面提交信息
+    @PostMapping("login")
+    public String loginInfo(@RequestParam("username") String uName,
+                            @RequestParam("password") String password,
+                            HttpSession session){
+        User user = searchService.UserLogin(uName, password);
+        session.setMaxInactiveInterval(3600); //括号内数字单位是秒，表示登录的持续时间
+        if(user!=null){
+            session.setAttribute("user", user );
+            return "redirect:/search/main";
+        }else{
+            return "redirect:/search/login";
+        }
     }
 
     // 学生管理系统
@@ -85,13 +159,13 @@ public class StudentController {
         PageInfo<Student> studentPageInfo = null;
 
         if(clsid == 0 && stu_name.isEmpty()){
-            studentPageInfo = studentService.findAllStudent(pageIndex, pageSize);
+            studentPageInfo = searchService.findAllStudent(pageIndex, pageSize);
         }
         else if(clsid == 0 && !stu_name.isEmpty()){
-            studentPageInfo = studentService.findStudentByStuName(pageIndex, pageSize, stu_name);
+            studentPageInfo = searchService.findStudentByStuName(pageIndex, pageSize, stu_name);
         }
         else{
-            studentPageInfo = studentService.findStudentByClsIDStuName(pageIndex, pageSize, clsid, stu_name);
+            studentPageInfo = searchService.findStudentByClsIDStuName(pageIndex, pageSize, clsid, stu_name);
         }
 
         List<ClassInfo> classes = classInfoService.findAllClassInfo();
@@ -128,14 +202,14 @@ public class StudentController {
 
         // 2.保存文件名到数据库里
         student.setStu_image_url(fileName);
-        studentService.addStudentInfo(student);
+        searchService.addStudentInfo(student);
         return "redirect:/stu/list";
     }
 
     // 修改学生信息页面
     @GetMapping("update/{id}")
     public String updateStudent(@PathVariable("id") Integer stuid, Model model){
-        Student student = studentService.findStudentByID(stuid);
+        Student student = searchService.findStudentByID(stuid);
         List<ClassInfo> classes = classInfoService.findAllClassInfo();
         model.addAttribute("cls", classes);
         model.addAttribute("stu",student);
@@ -158,7 +232,7 @@ public class StudentController {
 
         // 2.保存文件名到数据库里
         student.setStu_image_url(fileName);
-        studentService.updateStudentByID(student);
+        searchService.updateStudentByID(student);
         return "redirect:/stu/list";
     }
 
@@ -168,8 +242,8 @@ public class StudentController {
                                 @RequestParam(value = "pageIndex",defaultValue = "1") Integer pageIndex,
                                 @RequestParam(value = "pageSize",defaultValue = "5") Integer pageSize,
                                 Model model){
-        studentService.deleteStudentByID(stuid);
-        PageInfo<Student> stuLists = studentService.findAllStudent(pageIndex, pageSize);
+        searchService.deleteStudentByID(stuid);
+        PageInfo<Student> stuLists = searchService.findAllStudent(pageIndex, pageSize);
         model.addAttribute("stus",stuLists);
         return "redirect:/stu/list";
     }
